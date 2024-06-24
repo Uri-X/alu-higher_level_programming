b801af4d37a5a85a71ef609cbd3b8cf5335d7950
Uri-x <philbertkuria@gmail.com>
2024-06-24 14:38:35 -0700

    edited a program that prints all possible different combinations of two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
-for tens_digit in range(9):  # tens_digit ranges from 0 to 8
+for tens_digit in range(10):  # tens_digit ranges from 0 to 9
     for units_digit in range(tens_digit + 1, 10):  # units_digit ranges from tens_digit + 1 to 9
-        if tens_digit != units_digit:
-            if tens_digit == 0 and units_digit == 1:
-                print("01", end=", ")
-            else:
-                print("{:d}{:d}".format(tens_digit, units_digit), end=", ")
-
-print("89")
+        if tens_digit == 8 and units_digit == 9:
+            print("89")
+        else:
+            print("{:d}{:d}".format(tens_digit, units_digit), end=", ")
