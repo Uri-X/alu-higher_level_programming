8c0102e9210bf43e8132a4059dc2387a0b5b7200
Uri-x <philbertkuria@gmail.com>
2024-06-24 15:30:03 -0700

    a function that computes a to the power of b and return the value

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def pow(a, b):
+    return a ** b
+
+
+# Example usage:
+if __name__ == "__main__":
+    print(pow(2, 3))  # 8
+    print(pow(5, 0))  # 1
+    print(pow(-2, 2)) # 4
