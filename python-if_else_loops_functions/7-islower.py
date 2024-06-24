8a3689d87760c52b30d90a694975cba72c66b231
Uri-x <philbertkuria@gmail.com>
2024-06-24 15:00:18 -0700

    function that checks for lowercase character

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -3,14 +3,3 @@
 
 def islower(c):
     return ord(c) >= ord('a') and ord(c) <= ord('z')
-
-
-# Test cases
-if __name__ == "__main__":
-    print(islower('a'))  # True
-    print(islower('z'))  # True
-    print(islower('A'))  # False
-    print(islower('Z'))  # False
-    print(islower('!'))  # False
-    print(islower('m'))  # True
-    print(islower('M'))  # False
