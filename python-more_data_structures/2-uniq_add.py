6cc9c824d613a942fcc95bb5e6a311c8b6a33139
Uri-x <philbertkuria@gmail.com>
2024-06-29 00:39:39 -0700

    edited file

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-# This line indicates that the script should be run using the Python 3 interpreter.
-
 def uniq_add(my_list=[]):
-    # Define a function named uniq_add that takes a list (my_list) as an argument.
-    # If no argument is provided, it defaults to an empty list.
-    
-    # Convert the list to a set to remove duplicate values and then sum the unique values.
     return sum(set(my_list))
