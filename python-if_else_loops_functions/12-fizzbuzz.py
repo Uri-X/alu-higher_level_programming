368ad8e43c4f2590638ebb5f92b7449e7df1558d
Uri-x <philbertkuria@gmail.com>
2024-06-29 00:46:53 -0700

    rewrote code

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-
 def fizzbuzz():
-    for num in range(1, 101):
-        if num % 3 == 0 and num % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif num % 3 == 0:
-            print("Fizz", end=" ")
-        elif num % 5 == 0:
-            print("Buzz", end=" ")
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz', end=' ')
+        elif i % 3 == 0:
+            print('Fizz', end=' ')
+        elif i % 5 == 0:
+            print('Buzz', end=' ')
         else:
-            print(num, end=" ")
-
-if __name__ == "__main__":
-    fizzbuzz()
-    print()  # To print a newline after the FizzBuzz output
+            print(i, end=' ')
