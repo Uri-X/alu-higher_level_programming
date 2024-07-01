e0b9737ce6ac1e198e867a6d83071fcf55fe9bd0
Uri-x <philbertkuria@gmail.com>
2024-07-01 15:45:16 -0700

    edited file

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def check_integer(value):
-    if not isinstance(value, int):
-        raise TypeError("Expected an integer")
-    else:
-        print(f"The value {value} is an integer")
+def raise_exception():
+    raise TypeError
