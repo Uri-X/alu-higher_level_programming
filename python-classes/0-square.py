e060ddc6e16c21e120978a3177deabe017548932
Uri-x <philbertkuria@gmail.com>
2024-07-03 11:23:12 -0700

    edited file

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
-"""Square Class
-
-This class don't do anything for the moment
-
 """
+Square Class
+
+This class represents a square, although it currently doesn't have any functionality.
+
+Attributes:
+    None
+
+Methods:
+    None
+"""
+
 class Square:
+    """
+    A class that represents a square.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
     pass
