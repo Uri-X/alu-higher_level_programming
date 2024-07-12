bd6d19ae9fbc714a031f3061c1803957acf251de
Uri-x <philbertkuria@gmail.com>
2024-07-12 07:34:19 -0700

    a function that returns a boolean

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-def is_kind_of_class(obj, a_class):
+"""
+provides function to check if an object is exactly an instance
+of a specified class.
+"""
+
+
+def is_same_class(obj, a_class):
     """
-    Checks if `obj` is the same class or inherit from `a_class`
+    Checks if `obj` is exactly an instance of the specified class.
 
     Args:
-        obj (any): The object to compare
-        a_class (any): The class to compare with the object
+        obj (any): The object to compare.
+        a_class (any): The class to compare with the object.
 
     Returns:
-        `True` if the object is an instance or inherit from the
-        specified class; otherwise `False`
+        bool: `True` if the object is exactly an instance of the
+        specified class; otherwise `False`.
     """
-
-    return isinstance(obj, a_class)
+    return
