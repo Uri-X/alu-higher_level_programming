90f19cba0ecccf763f8d57b294db0254c368514e
Uri-x <philbertkuria@gmail.com>
2024-07-12 07:22:13 -0700

    documented the function

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -15,8 +15,4 @@ def is_same_class(obj, a_class):
         bool: `True` if the object is exactly an instance of the
         specified class; otherwise `False`.
     """
-
-    if type(obj) == a_class:
-        return True
-
-    return False
+    return type(obj) is a_class
