409024d842387d20705bfcfb24b8270946e455ed
Uri-x <philbertkuria@gmail.com>
2024-07-12 15:12:49 -0700

    a class MyList that inherits from list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,24 +1,47 @@
-#!/usr/bin/python3
-"""
-Module: 1-my_list
+# tests/1-my_list.txt
 
-The MyList class has method to print the list in ascending order.
-"""
+# Test instantiation and methods of MyList
 
+>>> MyList = __import__('1-my_list').MyList
 
-class MyList(list):
-    """
-    MyList class inherits from list and provides additional functionality.
+>>> my_list = MyList()
+>>> my_list.append(1)
+>>> my_list.append(4)
+>>> my_list.append(2)
+>>> my_list.append(3)
+>>> my_list.append(5)
+>>> print(my_list)
+[1, 4, 2, 3, 5]
 
-    Methods:
-        print_sorted: Prints the list in ascending order.
-    """
+>>> my_list.print_sorted()
+[1, 2, 3, 4, 5]
 
-    def print_sorted(self):
-        """
-        Prints the list in ascending order.
+>>> print(my_list)
+[1, 4, 2, 3, 5]
 
-        This method sorts the list and prints it.
-        """
-        if issubclass(MyList, list):
-            print(sorted(self))
+# Test with negative numbers
+>>> my_list = MyList()
+>>> my_list.append(-1)
+>>> my_list.append(-4)
+>>> my_list.append(-2)
+>>> my_list.append(-3)
+>>> my_list.append(-5)
+>>> print(my_list)
+[-1, -4, -2, -3, -5]
+
+>>> my_list.print_sorted()
+[-5, -4, -3, -2, -1]
+
+>>> print(my_list)
+[-1, -4, -2, -3, -5]
+
+# Test with an empty list
+>>> my_list = MyList()
+>>> print(my_list)
+[]
+
+>>> my_list.print_sorted()
+[]
+
+>>> print(my_list)
+[]
