3389ec07c82702204b3d917722b64d8586fb44ef
Uri-x <philbertkuria@gmail.com>
2024-07-12 07:53:11 -0700

    an empty class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module: base_geometry
+
+This module defines the BaseGeometry class.
+"""
+
+class BaseGeometry:
+    """
+    An empty class representing a base geometry.
+    """
+    pass
