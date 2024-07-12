ad2c1ccb8d523f8db0fde0fec2a76c7dfa679ec1
Uri-x <philbertkuria@gmail.com>
2024-07-12 07:40:58 -0700

    a function that returns a boolean

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+Module: my_module
+
+This module contains functions for type checking in Python.
+"""
+
 def is_same_class(obj, a_class):
     """
     Checks if `obj` is exactly an instance of the specified class.
