605c9699f0412939caea074e667af67be8db6e31
Uri-x <philbertkuria@gmail.com>
2024-07-12 07:57:20 -0700

    a class BaseGeometry based on 5-base_geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module: base_geometry
+
+This module defines the BaseGeometry class.
+"""
+
+class BaseGeometry:
+    """
+    A base class for geometry operations.
+    """
+
+
+    def area(self):
+        """
+        Raises an exception with a message indicating that
+        the method area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
