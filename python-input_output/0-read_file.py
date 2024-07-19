639a330bf80c71e03c69f7e04ea6e842a0a8901a
Uri-x <philbertkuria@gmail.com>
2024-07-19 01:03:32 -0700

    added comments in code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-def number_of_lines(filename=""):
-    with open(filename, encoding='utf-8') as f:
-        return len(f.readlines())
+"""
+This module contains a function to read a text file and print its content to stdout.
+"""
+
+def read_file(filename=""):
+    """
+    Reads a UTF8 text file and prints its content to stdout.
+
+    Args:
+        filename (str): The path to the text file to be read. Defaults to an empty string.
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content)
