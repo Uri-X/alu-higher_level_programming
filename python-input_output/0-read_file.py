d8ef912ac2787011c0d61f2194062e56303cd4fc
Uri-x <philbertkuria@gmail.com>
2024-07-19 01:28:26 -0700

    added documentation in code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """
-Module that provides a function to read a text file and print its content.
+a function to read a text file and print its content.
 """
+
 
 def read_file(filename=""):
     """
-    Reads a text file (UTF8) and prints it to stdout.
-    
+    Reads (UTF8) and prints it to stdout.
+
     Args:
         filename (str): The name of the file to read. Defaults to an empty string.
-    
+
     Returns:
         None
     """
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read(), end='')
+
 
 if __name__ == "__main__":
     import sys
