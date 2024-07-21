5b2c97edaea8200844aad0dc5a53d6a2678c43d7
Uri-x <philbertkuria@gmail.com>
2024-07-21 01:41:45 -0700

    a class Student that defines a student by based on 10-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -22,3 +22,12 @@ class Student():
             return new_dict
         else:
             return self.__dict__
+
+    def reload_from_json(self, json):
+        for key in json:
+            if key == "first_name":
+                self.first_name = json[key]
+            if key == "last_name":
+                self.last_name = json[key]
+            if key == "age":
+                self.age = json[key]
