020afd70ba459a4029c40b19103b3414a697a89d
Uri-x <philbertkuria@gmail.com>
2024-07-21 00:52:47 -0700

    a function that writes a string to a text file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
+"""function that writes a string to text file"""
+
 
 def write_file(filename="", text=""):
-    """
-    Writes a string to a text file (UTF8) and returns the number of characters written.
-    
-    Parameters:
-    filename (str): The name of the file to write to.
-    text (str): The text to write to the file.
-    
-    Returns:
-    int: The number of characters written.
-    """
-    with open(filename, 'w', encoding='utf-8') as file:
-        return file.write(text)
+    """writes string to text file"""
+    with open(filename, 'w') as f:
+        return f.write(text)
