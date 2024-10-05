9437edb77075a6290a57d0da68d0c0be1bada1a8
Uri-x <philbertkuria@gmail.com>
2024-10-05 00:55:13 -0700

    a script that reads and prints the content of a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
-// Import the 'fs' (File System) module
 const fs = require('fs');
 
-// Get command line arguments
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-// Write the content to the specified file
-fs.writeFile(filePath, content, 'utf8', (err) => {
-  if (err) {
-    console.error("An error occurred:", err); // Print the error if any occurs
-  } else {
-    console.log("File written successfully!");
-  }
+fs.readFile(process.argv[2], function (err, data) {
+  err && console.log(err);
+  const content = data;
+  console.log(content.toString());
 });
-
