ab6f8f9d3dd4cf8a0a4767139d55d37459a164b9
Uri-x <philbertkuria@gmail.com>
2024-10-05 00:59:37 -0700

    a script that display the status code of a GET request

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-// script that displays the status code of a GET request
 const request = require('request');
-const myArgs = process.argv.slice(2);
-request(myArgs[0], function (err, response) {
+request(process.argv[2], function (err, res, body) {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
-    console.log('code: ' + response.statusCode);
+    console.log(`code: ${res.statusCode}`);
   }
 });
